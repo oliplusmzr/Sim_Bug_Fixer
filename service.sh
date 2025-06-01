bb10649dffa2d12576e4da20937e6c8734f8d8a8
oliplusmzr <96580492+oliplusmzr@users.noreply.github.com>
2025-06-01 20:37:22 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,17 +1,24 @@
 #!/system/bin/sh
+# SIM Bug Fix - Service Script
+
 MODDIR=${0%/*}
+LOGTAG="SIM_FIX"
+LOGFILE="/sdcard/simfix_log.txt"
 
-# Sonsuz döngü: her 5 saatte bir çalışacak
+log -p i -t $LOGTAG "Servis başlatıldı."
+
 while true; do
-  # Telefon görüşme kontrolü
-  in_call=$(dumpsys telephony.registry | grep -i "mCallState=2")
-  if [ -z "$in_call" ]; then
-    log -p i -t SIM_FIX "Telefon boşta, işlem başlatılıyor..."
-    sh $MODDIR/simfix.sh
-  else
-    log -p i -t SIM_FIX "Görüşme tespit edildi, işlem ertelendi."
-  fi
+    # mCallState=2 → aktif görüşme, mCallState=0 → görüşme yok
+    CALL_STATE=$(dumpsys telephony.registry | grep -m 1 'mCallState=' | awk -F= '{print $2}' | tr -d '\r')
 
-  # 5 saat bekle (18000 saniye)
-  sleep 18000
+    if [ "$CALL_STATE" = "0" ]; then
+        log -p i -t $LOGTAG "Görüşme yok, simfix.sh çalıştırılıyor."
+        echo "$(date) - SIMFIX çalıştı (Görüşme yok)" >> $LOGFILE
+        sh "$MODDIR/simfix.sh"
+        sleep 18000  # 5 saat uyku
+    else
+        log -p i -t $LOGTAG "Görüşme aktif, bekleniyor..."
+        echo "$(date) - Görüşme vardı, simfix iptal" >> $LOGFILE
+        sleep 60
+    fi
 done
