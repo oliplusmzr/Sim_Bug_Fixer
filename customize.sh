2c559fb248f7e86ccab3603038020ea70cd415c0
oliplusmzr <96580492+oliplusmzr@users.noreply.github.com>
2025-06-14 21:55:24 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,6 +5,13 @@ ui_print "
 ╚══╗╠╣╚╝║║╔══╝║║─╔╝╚╗─╚╗╔╣╔╗║╔╝║║╔═╬══╗╠══╗║
 ║╚═╝║║║║║║║──╔╣╠╦╝╔╗╚╗─║║║╚╝║║─║╚╩═╠══╝╠══╝║
 ╚═══╩╩╩╩╝╚╝──╚══╩═╝╚═╝─╚╝╚══╩╝─╚═══╩═══╩═══╝"
+MODDIR=${0%/*}
+chmod 755 $MODDIR/service.sh
+chmod 755 $MODDIR/post-fs-data.sh
+chmod 755 $MODDIR/simfix.sh
+chmod 755 $MODDIR/uninstall.sh
+chmod 755 $MODDIR/another-script.sh
+ui_print " Version 2.1.1"
 ##########################################################################################
 # MMT Extended Logic - Don't modify anything after this
 ##########################################################################################
